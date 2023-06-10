ea86c2626f9539fdf4d52fac9e298ce2c296bda5
Pradeep2888 <pradeeptiwari2888@gmail.com>
2023-06-10 22:03:54 +0530

    created first api with succeffuly connected with mongoDB fdata base

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+// const { registerRouter } = require("./routes/register.route");
+// const { loginRouter } = require("./routes/login.route");
+const cors = require("cors");
+const { connection } = require("./config/db");
+
+
+const app = express();
+app.use(cors()) 
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("welcome to api")
+})
+
+// app.use("/register",registerRouter)
+// app.use("/login",loginRouter)
+
+
+
+
+
+
+
+app.listen(8080, async () => {
+    try{
+        await connection
+        console.log("Connection to DB successfully")
+    }
+    catch(err){
+        console.log(err)
+        console.log("Error connecting to DB")
+    }
+    console.log(`Listening on PORT 8080`)
+})
